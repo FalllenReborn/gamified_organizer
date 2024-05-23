f1dbc3cb81d2d8e7a0d6ea8ac0b8cd8068758826
Falllen <seweryn735@gmail.com>
2024-05-23 12:27:10 +0200

    Adjusted sidebar

--- frontend/src/components/sidebar/Sidebar.js
+++ frontend/src/components/sidebar/Sidebar.js
@@ -4,6 +4,16 @@ import './sidebar.css';
 const Sidebar = ({ onReturnHome }) => {
   return (
     <div className="sidebar">
+      <div className="sidebar-top">
+        <label className="switch">
+          <input type="checkbox" />
+          <span className="slider round"></span>
+        </label>
+        <select className="language-dropdown">
+          <option value="english">🇺🇸 English</option>
+          <option value="polish">🇵🇱 Polish</option>
+        </select>
+      </div>
       <div className="sidebar-buttons">
         <button className="sidebar-button">Create new list</button>
         <button className="sidebar-button">Create new shop</button>
@@ -11,6 +21,7 @@ const Sidebar = ({ onReturnHome }) => {
       </div>
       <div className="sidebar-bottom-buttons">
         <button className="sidebar-button">Login</button>
+        <span className="divider"> / </span>
         <button className="sidebar-button">Register</button>
         <button className="sidebar-button" onClick={onReturnHome}>Return to Home page</button>
       </div>
